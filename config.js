e9a602a48ca2bf1629beb17ba9a528df625c0509
Tommygans <91420132+Tommygans@users.noreply.github.com>
2021-11-04 10:10:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 userbot = {
- owner: [ "6281261324817", "0" ],
+ owner: [ "6281261324817", "6285780589674", "6285922275443","6285279294060","6282251837792", "6285922012008", ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
@@ -12,7 +12,7 @@ userbot = {
    waits: "Sedang di proses",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
    packname: "RIXLE-BOT OFFICIAL",
-   author: 'Arifi Razzaq',
+   author: 'Arifi Tommy',
     setting: {
     admin: "only admin",
     group: "only group",
